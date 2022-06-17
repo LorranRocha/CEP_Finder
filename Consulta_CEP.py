e23e2776db68f547f685d012f6c01ec87eee2370
LorranRocha <youtubebizarro@hotmail.com>
2022-06-17 20:12:36 -0300

    atualização

--- Consulta_CEP.py
+++ Consulta_CEP.py
@@ -14,41 +14,48 @@ def limpar():
         os.system('clear')
     finally:
         os.system('cls')
-    
-limpar()
 
-print("Bem Vindo ao Consulta CEP!")
-
-time.sleep(4)
-
-limpar()
-
-
-cep = input("Digite o CEP desejado: ")
-
-
-if len(cep) != 8:
+def main():
     limpar()
-    print("O CEP {} é inválido! \nFavor inserir outro!".format(cep))
-    time.sleep(2)
-    
-    exit()
 
-api = requests.get("https://viacep.com.br/ws/{}/json/".format(cep))
 
-dados = api.json()
+    cep = input("Digite o CEP desejado: ")
 
-if 'erro' not in dados:
-    limpar()
-    print("OPA! CEP ENCONTRADO!!!!\n")
-    print("CEP: {}".format(dados['cep']))
-    print("Logradouro: {}".format(dados['logradouro']))
-    print("Bairro: {}".format(dados['bairro']))
-    print("Cidade: {}".format(dados['localidade']))
-    print("Estado: {}".format(dados['uf']))
-    print("\nCriado por: https://www.lorranrocha.com/\n")
 
-else:
-    limpar()
-    print("OPA! CEP NÃO ENCONTRADO!!!!\n")
+    if len(cep) != 8:
+        limpar()
+        print("O CEP {} é inválido! \nFavor inserir outro!".format(cep))
+        time.sleep(2)
+        
+        exit()
 
+    api = requests.get("https://viacep.com.br/ws/{}/json/".format(cep))
+
+    dados = api.json()
+
+    if 'erro' not in dados:
+        limpar()
+        print("OPA! CEP ENCONTRADO!!!!\n")
+        print("CEP: {}".format(dados['cep']))
+        print("Logradouro: {}".format(dados['logradouro']))
+        print("Bairro: {}".format(dados['bairro']))
+        print("Cidade: {}".format(dados['localidade']))
+        print("Estado: {}".format(dados['uf']))
+        print("\nCriado por: https://www.lorranrocha.com/\n")
+
+    else:
+        limpar()
+        print("OPA! CEP NÃO ENCONTRADO!!!!\n")
+
+    opcao = int(input("Deseja realizar outra consulta?\n1 - Sim\n2 - Não\n\nR:"))
+
+    if opcao == 1:
+        main()
+    else:
+        limpar()
+        print("Obrigado por usar nosso sistema!!!")
+        time.sleep(2)
+        exit()
+
+if __name__ == '__main__':
+    main()
